900264a14f5458bfdd6ca18b2ae3d1f248b00aaf
JaiVskY <jaismailforpics@gmail.com>
2026-03-26 01:30:01 +0530

    test commit

--- producer.py
+++ producer.py
@@ -16,9 +16,9 @@ def delivery_report(err,msg):
 
 order={
     "order_id":str(uuid.uuid4()),
-    "user":"Harry",
-    "Product":'chicken pizza',
-    "Quantity":2
+    "user":"Fred",
+    "Product":'chicken pizza pdele',
+    "Quantity":4
 }
 
 value=json.dumps(order).encode("utf-8")
